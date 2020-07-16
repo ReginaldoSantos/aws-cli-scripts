f39b61cc7a2c1b965de7307be85b41df285e18b2
ReginaldoSantos <reginaldo_email@yahoo.com.br>
2020-07-16 15:11:23 -0300

    dynamodb/dynamodb-fill-tables.sh: granting 'app1' access to DynamoDB via Google IdP

--- dynamodb/dynamodb-fill-tables.sh
+++ dynamodb/dynamodb-fill-tables.sh
@@ -33,3 +33,11 @@ aws dynamodb query --table-name Reply \
 --index-name PostedBy-Message-Index \
 --key-condition-expression "Id = :v_id and PostedBy = :v_user" \
 --expression-attribute-values  '{":v_id":{"S":"Amazon DynamoDB#DynamoDB Thread 1"},":v_user":{"S":"User A"} }'
+
+## Using Google as IdP and Role ARN to give app1 access to DynamoDb.
+# aws sts assume-role-with-web-identity \
+#     --duration-seconds 3600 \
+#     --role-session-name "app1" \
+#     --provider-id "www.google.com" \
+#     --role-arn arn:aws:iam::504163318178:role/DynamoDB-Google-IdP \
+#     --web-identity-token "Atza%7CIQEBLjAsAhRFiXuWpUXuRvQ9PZL3GMFcYevydwIUFAHZwXZXXXXXXXXJnrulxKDHwy87oGKPznh0D6bEQZTSCzyoCtL_8S07pLpr0zMbn6w1lfVZKNTBdDansFBmtGnIsIapjI6xKR02Yc_2bQ8LZbUXSGm6Ry6_BG7PrtLZtj_dfCTj92xNGed-CrKqjG7nPBjNIL016GGvuS5gSvPRUxWES3VYfm1wl7WTI7jn-Pcb6M-buCgHhFOzTQxod27L9CqnOLio7N3gZAGpsp6n1-AJBOCJckcyXe2c6uD0srOJeZlKUm2eTDVMf8IehDVI0r1QOnTV6KzzAI3OY87Vd_cVMQ"
