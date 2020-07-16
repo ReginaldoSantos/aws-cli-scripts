1b98bb7b67c84296f7161383be41d75062d2782d
ReginaldoSantos <reginaldo_email@yahoo.com.br>
2020-07-16 13:32:37 -0300

    dynamodb/dynamodb-fill-tables.sh: new query in Reply table with index and projections

--- dynamodb/dynamodb-fill-tables.sh
+++ dynamodb/dynamodb-fill-tables.sh
@@ -25,3 +25,9 @@ aws dynamodb query --table-name Forum --key-conditions file://key-conditions-for
 
 # Query with key (HASH: mandatory for query), RANGE and comparison operator
 aws dynamodb query --table-name Thread --key-conditions file://key-conditions-thread.json
+
+# Query with local secondary index to evaluate projections: keys only
+aws dynamodb query --table-name Reply \
+--index-name PostedBy-Message-Index \
+--key-condition-expression "Id = :v_id and PostedBy = :v_user" \
+--expression-attribute-values  '{":v_id":{"S":"Amazon DynamoDB#DynamoDB Thread 1"},":v_user":{"S":"User A"} }'
